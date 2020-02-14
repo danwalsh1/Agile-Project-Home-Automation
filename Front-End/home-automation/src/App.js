5e7c146426390604656b3cbab628a4b911987ec1
walshd9 <walshd9@coventry.ac.uk>
2020-02-14 10:46:36 +0000

    added light control componenet

--- Front-End/home-automation/src/App.js
+++ Front-End/home-automation/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LightControl from './componenets/lightControl';
 
 function App() {
   return (
     <div className="App">
       <div>
+        <LightControl />
       </div>
     </div>
   );
